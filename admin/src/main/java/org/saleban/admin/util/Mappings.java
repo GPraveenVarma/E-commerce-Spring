ee6cc6e2e17ffb31440dc3eb8d974fc29214c853
GPraveenVarma <105558430+GPraveenVarma@users.noreply.github.com>
2023-07-24 15:55:16 -0400

    Update Mappings.java

--- admin/src/main/java/org/saleban/admin/util/Mappings.java
+++ admin/src/main/java/org/saleban/admin/util/Mappings.java
@@ -3,11 +3,10 @@ package org.saleban.admin.util;
 public final class Mappings {
     //Constants
     public static final String HOME = "/";
-    public static final String HOME2 = "/";
+
     public static final String LOGIN = "/login";
     public static final String INDEX = "/home";
-    public static final String INDEX4 = "/home";
-    public static final String INDEX3 = "/home";
+
     public static final String LOGOUT = "/logout";
     public static final String ADD_PRODUCT = "/product";
 
