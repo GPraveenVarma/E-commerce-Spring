304c39f3994e6140aa0b5161e4a875dd5f3d5d7a
Praveen varma <g.praveenvarma96@gmail.com>
2023-07-24 15:45:30 -0400

    mappings changes

--- admin/src/main/java/org/saleban/admin/util/Mappings.java
+++ admin/src/main/java/org/saleban/admin/util/Mappings.java
@@ -6,6 +6,8 @@ public final class Mappings {
     public static final String HOME2 = "/";
     public static final String LOGIN = "/login";
     public static final String INDEX = "/home";
+    public static final String INDEX2 = "/home";
+    public static final String INDEX3 = "/home";
     public static final String LOGOUT = "/logout";
     public static final String ADD_PRODUCT = "/product";
 
